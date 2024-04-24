e6b7e75586012eda55796ea81023af53397f1611
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 17:16:53 -0300

    :lipstick: Melhoria UI UX

--- src/components/SwitcherTheme.tsx
+++ src/components/SwitcherTheme.tsx
@@ -9,7 +9,7 @@ export function ThemeSwitcher() {
       role="button"
       aria-label="Toggle Theme"
       onClick={toggleTheme}
-      className={`fixed bottom-6 w-14 h-14 border-2 dark:border-gray-600 rounded-full flex items-center justify-center z-[99] shadow-md sm:left-4 max-sm:bottom-28 max-sm:right-4 max-sm:w-12 max-sm:h-12 dark:bg-light bg-dark transition-colors`}
+      className={`fixed left-8 bottom-6 w-14 h-14 border-2 dark:border-gray-600 rounded-full flex items-center justify-center z-[9999] shadow-md   dark:bg-light bg-dark transition-colors`}
     >
       {colorTheme === "dark" ? (
         <Sun size={24} className="text-dark" />
