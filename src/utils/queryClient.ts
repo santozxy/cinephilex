169ec640d1bb9ff1a5daf9d5cfa0d40d47948c38
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 17:17:32 -0300

    :sparkles: Adicionar opções padrão ao queryClient

--- src/utils/queryClient.ts
+++ src/utils/queryClient.ts
@@ -1,3 +1,16 @@
 import { QueryClient } from "@tanstack/react-query";
+const minute = 1000 * 60;
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+      refetchOnMount: false,
+      retry: false,
+      staleTime: 10 * minute,
+      gcTime: 10 * minute,
+    },
+  },
+});
