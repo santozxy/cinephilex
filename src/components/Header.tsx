826ecdca7038a3b911c6345fcee1610816f9778e
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 11:01:50 -0300

    :sparkles: add links de navegação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,80 @@
+import { Link } from "@tanstack/react-router";
+import { Film, Home, Search, Tv } from "lucide-react";
+
 export function Header() {
   return (
-    <div className="p-2 bg-dark">
-      <h1 className="text-2xl text-purple-700">Header</h1>
+    <div className="p-2 bg-dark flex items-center justify-between w-full h-20">
+      <h1 className="text-3xl text-primary">CinephileX</h1>
+      <nav className="flex gap-8 pr-5">
+        <Link to="/home" className="py-2 no-underline">
+          {({ isActive }) => {
+            return (
+              <div className="flex items-center justify-center gap-3">
+                <Home
+                  size={24}
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                />
+                <p
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                >
+                  Home
+                </p>
+              </div>
+            );
+          }}
+        </Link>
+        <Link to="/movies" className="py-2 no-underline">
+          {({ isActive }) => {
+            return (
+              <div className="flex items-center justify-center gap-3">
+                <Film
+                  size={24}
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                />
+                <p
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                >
+                  Movies
+                </p>
+              </div>
+            );
+          }}
+        </Link>
+        <Link to="/series" className="py-2 no-underline">
+          {({ isActive }) => {
+            return (
+              <div className="flex items-center justify-center gap-3">
+                <Tv
+                  size={24}
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                />
+                <p
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                >
+                  Series
+                </p>
+              </div>
+            );
+          }}
+        </Link>
+        <Link to="/search" className="py-2 no-underline">
+          {({ isActive }) => {
+            return (
+              <div className="flex items-center justify-center gap-3">
+                <Search
+                  size={24}
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                />
+                <p
+                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                >
+                  Search
+                </p>
+              </div>
+            );
+          }}
+        </Link>
+      </nav>
     </div>
   );
 }
