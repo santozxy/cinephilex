d76224df10b4e0bde5005d7e37b03c96ceed2041
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 14:09:50 -0300

    :sparkles: Add componentes CarrouselCards e CarrouselSlide

---
+++ src/components/CarrouselCards.tsx
@@ -0,0 +1,71 @@
+import { Link } from "@tanstack/react-router";
+import { Movie } from "../services/movies/moviesDTO";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+interface ListCardsProps {
+  title: string;
+  data: Movie[];
+  swipeable?: boolean;
+  showDots?: boolean;
+  autoplay?: boolean;
+}
+
+const imageURL = import.meta.env.VITE_IMG;
+
+export default function ListCards({
+  title = "",
+  data,
+  swipeable = true,
+  showDots = false,
+  autoplay = false,
+}: ListCardsProps) {
+  return (
+    <div>
+      <h1 className="lg:text-2xl md:text-xl max-sm:text-lg text-dark dark:text-light font-medium p-3">
+        {title}
+      </h1>
+      <Carousel
+        swipeable={swipeable}
+        draggable={true}
+        showDots={showDots}
+        responsive={{
+          desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 5,
+          },
+          tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 4,
+          },
+          mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 2,
+          },
+        }}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlay={autoplay}
+        autoPlaySpeed={4000}
+        keyBoardControl={true}
+        customTransition="transition-all ease-in-out"
+        transitionDuration={100}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="pr-4"
+      >
+        {data.map((movie) => (
+          <div key={movie.id}>
+            <Link className="relative">
+              <img
+                src={imageURL + movie.poster_path}
+                alt={movie.title}
+                className="object-contain w-full rounded-md shadow-lg transition-all duration-400 ease-in-out transform hover:scale-105"
+              />
+            </Link>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
---
+++ src/components/CarrouselSlide.tsx
@@ -0,0 +1,70 @@
+import { Link } from "@tanstack/react-router";
+import { Movie } from "../services/movies/moviesDTO";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+interface ListCardsProps {
+  data: Movie[];
+  swipeable?: boolean;
+  showDots?: boolean;
+  autoplay?: boolean;
+}
+
+const imageURL = import.meta.env.VITE_IMG;
+
+export default function ListCards({
+  data,
+  swipeable = true,
+  showDots = false,
+  autoplay = true,
+}: ListCardsProps) {
+  return (
+    <div>
+      <Carousel
+        swipeable={swipeable}
+        draggable={true}
+        showDots={showDots}
+        responsive={{
+          desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+          },
+          tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+          },
+          mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+          },
+        }}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlay={autoplay}
+        autoPlaySpeed={5000}
+        keyBoardControl={true}
+        customTransition="transition-all"
+        transitionDuration={1000}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+      >
+        {data.map((movie) => (
+          <div key={movie.id}>
+            <Link className="relative">
+              <img
+                src={imageURL + movie.backdrop_path}
+                alt={movie.title}
+                className="object-cover w-full h-[35rem] max-sm:h-96 rounded-md shadow-lg "
+              />
+              <div className="absolute w-full bottom-0 left-0 right-0 bg-dark bg-opacity-60 text-light p-1 text-xs font-medium z-[99]">
+                <h2 className="lg:text-2xl md:text-xl max-sm:text-lg text-dark dark:text-light font-medium p-3">
+                  {movie.title}
+                </h2>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
