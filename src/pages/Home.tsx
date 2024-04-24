f2d64c45ec9918e1b75303b9172f350236197a52
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 11:52:55 -0300

    :sparkles: Adicionar consulta aos filmes mais bem avaliados na página inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import { getTopRatedMovies } from "../services/movies/movies";
+import { useQuery } from "@tanstack/react-query";
+import {ListCards} from "@components";
+
 export function Home() {
+  const topRatedMovies = useQuery({
+    queryKey: ["topRatedMovies"],
+    queryFn: () => getTopRatedMovies(),
+    enabled: true,
+    initialData: undefined,
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchInterval: false,
+  });
+
+  if (topRatedMovies.isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="bg-light w-full">
-      <h1>Home</h1>
+    <div className="bg-light w-full h-full">
+      {topRatedMovies.data && (
+        <ListCards
+          title="Top Rated Movies"
+          data={topRatedMovies.data?.results ?? []}
+        />
+      )}
     </div>
   );
 }
