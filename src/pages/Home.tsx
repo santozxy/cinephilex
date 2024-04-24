1aeb7974c69f6ec3b9a58a444f3c0c431b412bb0
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 14:12:17 -0300

    :sparkles: Add consultas de categorias de filmes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
-import { getTopRatedMovies } from "../services/movies/movies";
+import {
+  getNowPlayingMovies,
+  getPopularMovies,
+  getTopRatedMovies,
+} from "../services/movies/movies";
 import { useQuery } from "@tanstack/react-query";
-import {ListCards} from "@components";
+import { CarrouselCards, CarrouselSlide, Loading } from "@components";
+import { Separator } from "@components";
 
 export function Home() {
   const topRatedMovies = useQuery({
@@ -12,18 +17,57 @@ export function Home() {
     refetchOnWindowFocus: false,
     refetchInterval: false,
   });
-
-  if (topRatedMovies.isLoading) {
-    return <div>Loading...</div>;
-  }
+  const popularMovies = useQuery({
+    queryKey: ["popularMovies"],
+    queryFn: () => getPopularMovies(),
+    enabled: true,
+    initialData: undefined,
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchInterval: false,
+  });
+  const nowPlayingMovies = useQuery({
+    queryKey: ["nowPlayingMovies"],
+    queryFn: () => getNowPlayingMovies(),
+    enabled: true,
+    initialData: undefined,
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchInterval: false,
+  });
 
   return (
-    <div className="bg-light w-full h-full">
-      {topRatedMovies.data && (
-        <ListCards
-          title="Top Rated Movies"
-          data={topRatedMovies.data?.results ?? []}
-        />
+    <div className="w-full h-full flex flex-col p-4 mt-24">
+      {nowPlayingMovies.isLoading ? (
+        <Loading />
+      ) : (
+        nowPlayingMovies.data && (
+          <CarrouselSlide
+            data={nowPlayingMovies.data?.results ?? []}
+          />
+        )
+      )}
+      <Separator />
+      {popularMovies.isLoading ? (
+        <Loading />
+      ) : (
+        popularMovies.data && (
+          <CarrouselCards
+            title="Popular Movies"
+            data={popularMovies.data?.results ?? []}
+          />
+        )
+      )}
+      <Separator />
+      {topRatedMovies.isLoading ? (
+        <Loading />
+      ) : (
+        topRatedMovies.data && (
+          <CarrouselCards
+            title="Top rated movies"
+            data={topRatedMovies.data?.results ?? []}
+          />
+        )
       )}
     </div>
   );
