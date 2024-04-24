477c7f2845e116338684d5577830a899e19c3f0a
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 11:52:06 -0300

    :wrench: :bricks: Adicionar componente ListCards e ajustar importações

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,9 @@ import { Header } from "@components";
 
 export default function Layout() {
   return (
-    <>
+    <div className="flex flex-col gap-10">
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Header } from "./Header";
 import { SearchBar } from "./SearchBar";
+import { Loading } from "./Loading";
 
 const Layout = React.lazy(() => import("./Layout"));
+const ListCards = React.lazy(() => import("./ListCards"));
 
-export { Header, SearchBar, Layout };
+export { Header, SearchBar, Layout, Loading, ListCards };
