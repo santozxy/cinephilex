88f131733b0e5bf47ad15a630b0f5de97b9f6ed4
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-24 14:07:02 -0300

    :lipstick: Ajustar estilos responsividade no modo escuro

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,21 @@ import { Film, Home, Search, Tv } from "lucide-react";
 
 export function Header() {
   return (
-    <div className="p-2 bg-dark flex items-center justify-between w-full max-sm:flex-col h-20 max-sm:h-28 shadow-lg">
+    <div className="p-2  flex items-center justify-between w-full max-sm:flex-col px-4 h-20 max-sm:h-28 shadow-xl fixed top-0 z-[99999] bg-dark">
       <h1 className="lg:text-3xl md:text-2xl max-sm:text-2xl text-primary">
         CinephileX
       </h1>
-      <nav className="flex gap-8 pr-5 justify-center items-center">
+      <nav className="flex gap-8  justify-center items-center">
         <Link to="/home" className="py-2 no-underline">
           {({ isActive }) => {
             return (
               <div className="flex items-center justify-center gap-3">
                 <Home
                   size={24}
-                  className={`text-lg ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-lg ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 />
                 <p
-                  className={`text-lg max-[450px]:hidden ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-lg max-[450px]:hidden ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 >
                   Home
                 </p>
@@ -31,10 +31,10 @@ export function Header() {
               <div className="flex items-center justify-center gap-3">
                 <Film
                   size={24}
-                  className={`text-lg ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-lg ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 />
                 <p
-                  className={`text-lg  max-[450px]:hidden ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-lg  max-[450px]:hidden ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 >
                   Movies
                 </p>
@@ -48,10 +48,10 @@ export function Header() {
               <div className="flex items-center justify-center gap-3">
                 <Tv
                   size={24}
-                  className={`text-lg ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-lg ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 />
                 <p
-                  className={`text-lg max-[450px]:hidden ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-lg max-[450px]:hidden ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 >
                   Series
                 </p>
@@ -65,10 +65,10 @@ export function Header() {
               <div className="flex items-center justify-center gap-3">
                 <Search
                   size={24}
-                  className={`text-xl ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-xl ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 />
                 <p
-                  className={`text-xl max-[450px]:hidden ${isActive ? "text-primary" : "text-light"}`}
+                  className={`text-xl max-[450px]:hidden ${isActive ? "text-primary" : "dark:text-light text-dark"}`}
                 >
                   Search
                 </p>
