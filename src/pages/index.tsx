9bedf3e71c41671df82d6d58cb5f192d287187ee
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-04-24 22:17:46 -0300

    :sparkles: Adicionar página de séries com séries populares e séries mais bem avaliadas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { NotFound } from "./NotFound";
 import { Home } from "./Home";
-import { Movies } from "./Movies";
-import { Series } from "./Series";
+import { Movies } from "./Movies/Movies";
+import { Series } from "./TV/Series";
 import { Search } from "./Search";
 
 export { NotFound, Home, Movies, Series, Search };
