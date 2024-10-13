2ab1299a47539216011118bbb749dc005544646e
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-10-13 16:33:36 -0300

    :sparkles: feat: Configurando domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["lucide-react"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/w200/*",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/w500/*",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/original/*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
