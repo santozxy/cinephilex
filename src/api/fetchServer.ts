e0ddbc8be54cc61998678a3d1709b1c5116fbec5
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-10-13 14:59:03 -0300

    :sparkles: feat: Adicionando mais variaveis de dev

--- src/api/fetchServer.ts
+++ src/api/fetchServer.ts
@@ -3,8 +3,8 @@ import { env } from "@/env";
 const ACESS_TOKEN = env.NEXT_PUBLIC_API_ACESS_TOKEN;
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
-export async function fetchServer(endpoint: string, options: RequestInit = {}) {
-  const url = `${TMDB_BASE_URL}${endpoint}`;
+export async function fetchServer(path: string, options: RequestInit = {}) {
+  const url = `${TMDB_BASE_URL}${path}`;
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${ACESS_TOKEN}`,
--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,9 @@ import zod from "zod";
 export const envSchema = zod.object({
   NEXT_PUBLIC_API_TMDB_KEY: zod.string(),
   NEXT_PUBLIC_API_ACESS_TOKEN: zod.string(),
+  NEXT_PUBLIC_BASE_URL: zod.string(),
+  NEXT_PUBLIC_IMAGE_ORIGINAL: zod.string(),
+  NEXT_PUBLIC_IMAGE_W500: zod.string(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
