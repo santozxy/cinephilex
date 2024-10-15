eae592428e77719f710925ff0908206d416ab95a
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-10-14 21:59:41 -0300

    :sparkles: feat: Melhorias UI UX

--- src/app/(cinephilex)/layout.tsx
+++ src/app/(cinephilex)/layout.tsx
@@ -5,11 +5,11 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] bg-background lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen w-full md:grid-cols-[200px_1fr] bg-background lg:grid-cols-[260px_1fr]">
       <Sidebar />
       <div className="flex flex-col">
         <Header />
-        <div className="p-4 lg:gap-6 lg:p-6">{children}</div>
+        <div className="py-4">{children}</div>
       </div>
     </div>
   );
--- src/app/(cinephilex)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FeaturedMoviesCarousel } from "./components/featured-movies-carousel";
-
-export default async function Home() {
-  return (
-    <main>
-      <FeaturedMoviesCarousel />
-    </main>
-  );
-}
